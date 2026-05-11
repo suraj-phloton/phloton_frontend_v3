4f28f9b4a29286f1489e60c3b7dc2bf3b4404e8b
suraj-phloton <suraj@phloton.com>
2026-05-11 10:39:15 +0530

    Update admin_dashboard.py

--- users_ui/admin/admin_dashboard.py
+++ users_ui/admin/admin_dashboard.py
@@ -37,6 +37,10 @@ def drawAdminDashboard():
             st.Page(f"{current_dir}/units/unit_22.py", title="Unit 22",icon="🛜"),
             st.Page(f"{current_dir}/units/unit_23.py", title="Unit 23",icon="🛜"),
             st.Page(f"{current_dir}/units/unit_24.py", title="Unit 24",icon="🛜"),
+            st.Page(f"{current_dir}/units/unit_21.py", title="Unit 25",icon="🛜"),
+            st.Page(f"{current_dir}/units/unit_22.py", title="Unit 26",icon="🛜"),
+            st.Page(f"{current_dir}/units/unit_23.py", title="Unit 27",icon="🛜"),
+            st.Page(f"{current_dir}/units/unit_24.py", title="Unit 28",icon="🛜"),
         ]
     }
     pg = st.navigation(pages)
